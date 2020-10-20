9a6c99080a6e100f7614c8c6931620a0b6b1abdb
Jin_Hwan <astrowh@naver.com>
2020-10-20 13:47:08 +0900

    S03P31A406-23 swagger 추가

--- backend/l00moon1ta/settings.py
+++ backend/l00moon1ta/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    # swagger
+    'drf_yasg',
+    
     # cors, restframework
     'corsheaders',
     'rest_framework',
