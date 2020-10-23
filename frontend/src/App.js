acb1eabf62429b63f5bdda5d530c979debb60c50
jhj9109 <jhj91_09@naver.com>
2020-10-23 13:59:43 +0900

    Add react-router-dom Library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 // import './App.css';
 import { Helmet } from 'react-helmet-async';
+import { Route, Switch } from 'react-router-dom';
+import MainPage from './pages/MainPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
   return (
@@ -8,6 +11,10 @@ function App() {
       <Helmet>
         <title>100moon1ta</title>
       </Helmet>
+      <Switch>
+        <Route component={MainPage} path='/' exact />
+        <Route component={NotFoundPage} path='*' />
+      </Switch>
     </div>
   );
 }
---
+++ frontend/src/pages/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+const MainPage = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Main | 100moon1ta</title>
+      </Helmet>
+      Main Page
+    </>
+  );
+};
+
+export default MainPage;
