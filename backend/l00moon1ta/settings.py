984b28211d8a6dc38fa6f7c64f52d08639fd613c
Jin_Hwan <astrowh@naver.com>
2020-10-30 11:54:09 +0900

    S03P31A406-67 #Comment Add  email custom template

--- backend/l00moon1ta/settings.py
+++ backend/l00moon1ta/settings.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = 'l00moon1ta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
