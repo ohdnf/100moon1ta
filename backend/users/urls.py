1e9c8f8174520e8cf997242e8eac16fdca2762db
Jin_Hwan <astrowh@naver.com>
2020-11-02 12:16:03 +0900

    S03P31A406-73 #Comment 좋아요 기능 추가

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
-from .views import StaffManagement, BanManagement, Nickname, GitHubLogin, Record, Bookmark
+from .views import StaffManagement, BanManagement, Nickname, GitHubLogin, Record, Bookmark, Like
 
 urlpatterns = [
     path('signup/', include('rest_auth.registration.urls')),
     path('nickname-duplicated/<str:nickname>/', Nickname.as_view()),
     path('social/', GitHubLogin.as_view()),
     path('record/', Record.as_view()),
-    path('bookmark/', Bookmark.as_view()),
+    path('bookmark/', Bookmark.as_view()),   #url(r'^workers/$', login_required(login_url='')(views.RootWorkerView.as_view()))  이런 방식 가능
+    path('like/',Like.as_view()),
     path('mng/staff/<int:uid>/',StaffManagement.as_view()),
     path('mng/ban/<int:uid>/', BanManagement.as_view()),
     path('', include("rest_auth.urls")),
--- backend/users/views.py
+++ backend/users/views.py
@@ -8,8 +8,10 @@ from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from django.urls import reverse
 from rest_auth.registration.views import SocialLoginView
 
-from games.models import GameHistory
+from games.models import GameHistory, Source
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.db.models import Sum
 # Create your views here.
 
@@ -20,6 +22,8 @@ class Nickname(APIView):
             return Response({"possible" : not CustomUser.objects.filter(username=nickname).exists()})   
         # else:
         #     return Res
+
+# login 안되어있으면 404
 class Record(APIView):
     def get(self, request):
         total_point = GameHistory.objects.filter(user=request.user).aggregate(Sum('points'))
@@ -28,12 +32,29 @@ class Record(APIView):
         else:
             return Response('플레이한 게임이 없습니다.', status=404)
 
+@method_decorator(login_required, name='get') # login으로 redirect 시킴, login_url='/example url you want redirect/' 로 지정가능
 class Bookmark(APIView):
     def get(self, request):
-        User = get_user_model()
-        user = get_object_or_404(User, pk=request.user.id)
+        # User = get_user_model()
+        # user = get_object_or_404(User, pk=request.user.id) # 유저가 없으면 로그인이 안되므로 필요없음
+        user = request.user
         bookmark = user.subscribed_source
-        return Response({"sources" : bookmark.values()})     
+        return Response({"sources" : bookmark.values()})   
+
+# login 필요
+@method_decorator(login_required, name='post')
+class Like(APIView):
+    def post(self,request):
+        sid = request.POST.get('source_id')
+        user = request.user
+        source = get_object_or_404(Source, pk=sid)
+        if source.likers.filter(pk=user.pk).exists():
+            source.likers.remove(user)
+            like_status = "좋아요 취소"
+        else:
+            source.likers.add(user)
+            like_status = "좋아요!"
+        return Response({"sources" : like_status})   
 
 class GitHubLogin(SocialLoginView):
     adapter_class = github_views.GitHubOAuth2Adapter
@@ -45,6 +66,7 @@ class GitHubLogin(SocialLoginView):
         # must be absolute:
         return self.request.build_absolute_uri(reverse('github_callback'))
 
+# 권한 세부화
 class StaffManagement(APIView):
     def patch(self, request, uid):
         if request.user.is_superuser:
@@ -53,6 +75,7 @@ class StaffManagement(APIView):
             staff.save()
         return  Response({"possible" : request.user.is_superuser })
 
+# 권한 세부화
 class BanManagement(APIView):
     def patch(self, request, uid):
         if request.user.is_staff:
