27507d63b8e4ef49489d6a1dc6dbfd959919274b
jhj9109 <jhj91_09@naver.com>
2020-11-03 14:47:58 +0900

    Update 소스 목록 페이지 반영

--- frontend/src/pages/GamePage.js
+++ frontend/src/pages/GamePage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-
+import GameContainer from '../containers/game/GameContainer'
 const GamePage = () => {
   return (
     <>
       <Helmet>
         <title>GamePage | 100moon1ta</title>
       </Helmet>
-      <div>Game Page</div>
+      <GameContainer />
     </>
   );
 };
