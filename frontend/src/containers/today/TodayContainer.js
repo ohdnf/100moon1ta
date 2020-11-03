5592745084f3fc9c884ac3c58f787bf7626b0d9f
jhj9109 <jhj91_09@naver.com>
2020-11-03 14:45:34 +0900

    Update 오늘의 타자 페이지 목업 구현

--- frontend/src/containers/today/TodayContainer.js
+++ frontend/src/containers/today/TodayContainer.js
@@ -9,13 +9,19 @@
 // 히트맵
 
 import React, { useState } from 'react';
-import Source from '../../components/today/Source'
+import Source from '../../components/today/Source';
+import Summary from '../../components/today/Summary';
+import Keyboard from '../../components/today/Keyboard';
+import Button from '../../components/common/Button';
+
 const TodayContainer = () => {
   return (
     <>
-      <Source
-
-      />
+      <Source />
+      <Summary />
+      <Keyboard />
+      <Button> 다시 연습 하기 </Button>
+      <Button> 다음 소스 연습 </Button>
     </>
   );
 };
