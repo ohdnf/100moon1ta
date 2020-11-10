bc1d8fc796ba7f2b3e91bc47d25464c6c1eba8fa
장현준 <jhj91_09@naver.com>
2020-11-10 00:42:28 +0000

    Delete backend 폴더

--- backend/users/adapter.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from allauth.account.adapter import DefaultAccountAdapter
-
-class CustomAccountAdapter(DefaultAccountAdapter):
-
-    def get_login_redirect_url(self, request):
-        path = "/api/v1/rest-auth/login/"
-        return path
