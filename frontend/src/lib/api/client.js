a1c7af1e617afc6b74ba22a744752de271fef9de
jhj9109 <jhj91_09@naver.com>
2020-11-10 13:30:25 +0900

    Update client.js | 로컬스토리이의 JWT를 axios 헤더에 설정 |

--- frontend/src/lib/api/client.js
+++ frontend/src/lib/api/client.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
+// import { useSelector } from 'react-redux';
 
 dotenv.config();
-const requestURL = process.env.REACT_APP_API_URL
-const apiURL = 'api/v1/'
+const requestURL = process.env.REACT_APP_API_URL;
+const apiURL = 'api/v1/';
 const client = axios.create({
-  baseURL: requestURL + apiURL
+  baseURL: requestURL + apiURL,
 });
 
+// JWT 헤더
+const token = localStorage.getItem('token') // 없으면 null
+if (token) {
+  console.log("token헤더에 설정합니다", token)
+  client.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+}
 export default client;
 
 /*
