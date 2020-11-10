ec0298a821bed3e168dc1fde5f754d43b3b6743c
jhj9109 <jhj91_09@naver.com>
2020-11-10 09:35:26 +0900

    Update 소스목록 페이지 | 서브라우트 => TypingContainer 연결 |

--- frontend/src/pages/GamePage.js
+++ frontend/src/pages/GamePage.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
 import { Helmet } from 'react-helmet-async';
 import GameContainer from '../containers/game/GameContainer'
+import TypingContainer from '../containers/typing/TypingContainer'
+// 위의것을 바꿔야한다
+
 const GamePage = () => {
   return (
     <>
       <Helmet>
         <title>GamePage | 100moon1ta</title>
       </Helmet>
-      <GameContainer />
+
+      {/* <GameContainer /> */}
+      <Route path="/games" exact component={GameContainer}/>
+      <Route inputCode={"code is my life"} path="/games/:gameId" component={TypingContainer}/>
     </>
   );
 };
