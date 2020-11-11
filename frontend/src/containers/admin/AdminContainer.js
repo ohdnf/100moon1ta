12da1302f3a882b7c915385df8006d2fc1ffaba1
Jin_Hwan <astrowh@naver.com>
2020-11-11 17:22:25 +0900

    S03P31A406-87 #Comment 관리자 페이지 기능 구현

---
+++ frontend/src/containers/admin/AdminContainer.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import UserList from '../../components/admin/UserList';
+
+const AdminContainer = () => {
+  return (
+    <>
+      <UserList />
+    </>
+  );
+};
+
+export default AdminContainer;
