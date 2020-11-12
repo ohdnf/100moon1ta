6bff9c096549e2d9651069f4d0cf7c0650243644
jhj9109 <jhj91_09@naver.com>
2020-11-12 15:20:01 +0900

    Chorus prettier 적용

--- frontend/src/components/today/Source.js
+++ frontend/src/components/today/Source.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import TimerContainer from '../../containers/typing/TimerContainer';
 import Keyboard from './Keyboard';
-import Summary from './Summary'
+import Summary from './Summary';
 // import { useHistory } from 'react-router-dom';
 
 const SourceBlock = styled.div`
@@ -41,7 +41,7 @@ const LinkDiv = styled.div`
 `;
 const TypingWindow = styled.pre`
   .not-visit {
-      color: grey;
+    color: grey;
   }
   .visit {
     color: black;
@@ -68,64 +68,65 @@ const Source = ({ game, chList }) => {
   };
   const [cursor, setCursor] = useState(0);
 
-	const [start, setStart] = useState();
-  const [end , setEnd] = useState();
+  const [start, setStart] = useState();
+  const [end, setEnd] = useState();
 
   const [point, setPoint] = useState();
-  
+
   let indention = 0;
-  
+
   const handleKeyPress = (e) => {
-		if (!start) {
-			setStart(new Date());
-		}
-		if (cursor === chList.length) {  // 종료시점
-			if (e.key === 'Enter') {
+    if (!start) {
+      setStart(new Date());
+    }
+    if (cursor === chList.length) {
+      // 종료시점
+      if (e.key === 'Enter') {
         let dtEnd = new Date();
-        setPoint(100)  // 게임 종료 시 점수 반환, 정확도랑 속도 추가
-        setEnd((dtEnd-start)/1000)
-			}
-		} 
-    if (cursor < chList.length){
-			
-			if (e.key === chList[cursor]) {
-					typo[cursor] = 0;
-					setCursor(cursor + 1);
-			} else if (e.key !== chList[cursor]) {
-				typo[cursor] = 1;
-				if (typoMap[chList[cursor]]) {
-					++typoMap[chList[cursor]]
-				} else {
-					typoMap[chList[cursor]] = 1
-				}
-				setCursor(cursor + 1);
-			}
-			// console.log(cursor) // 언제 1++이 되는거지
-			if (chList[cursor] === '\n') { // 이전 값이 enter면
-				indention = cursor;
-				while (chList[indention+1] === ' ') {
-					++indention;
-				}
-				setCursor(indention+1);
-			}
-		}
+        setPoint(100); // 게임 종료 시 점수 반환, 정확도랑 속도 추가
+        setEnd((dtEnd - start) / 1000);
+      }
+    }
+    if (cursor >= chList.length) return; // 본문을 초과한 입력 무시
+    if (e.key === chList[cursor]) {
+      typo[cursor] = 0;
+      setCursor(cursor + 1);
+    } else if (e.key !== chList[cursor]) {
+      typo[cursor] = 1;
+      if (typoMap[chList[cursor]]) {
+        ++typoMap[chList[cursor]];
+      } else {
+        typoMap[chList[cursor]] = 1;
+      }
+      setCursor(cursor + 1);
+    }
+    // console.log(cursor) // 언제 1++이 되는거지
+    if (chList[cursor] === '\n') {
+      // 이전 값이 enter면
+      indention = cursor;
+      while (chList[indention + 1] === ' ') {
+        ++indention;
+      }
+      setCursor(indention + 1);
+    }
   };
   const deleteKeyDown = (e) => {
     if (e.key === 'Backspace') {
-        if (cursor > 0) {
-          setCursor(cursor - 1);
-				}
-				if (chList[cursor - 1] === ' '){
-					indention = cursor - 1;
-					while ( chList[indention] === ' ') {
-						--indention
-					}
-					if (chList[indention] === '\n') {
-						setCursor(indention-1) // 백스페이스 시 enter로 커서가 가지 않고 글자로 감
-					}
-				}
-		}
-  }
+      if (cursor > 0) {
+        setCursor(cursor - 1);
+      }
+      if (chList[cursor - 1] === ' ') {
+        indention = cursor - 1;
+        while (chList[indention] === ' ') {
+          --indention;
+        }
+        if (chList[indention] === '\n') {
+          setCursor(indention - 1); // 백스페이스 시 enter로 커서가 가지 않고 글자로 감
+        }
+      }
+    }
+  };
+  console.log(cursor, chList[cursor]);
   return (
     <SourceBlock>
       <div>오늘의 타자</div>
@@ -135,9 +136,13 @@ const Source = ({ game, chList }) => {
           <TagItem key={tag}>{tag}</TagItem>
         ))}
       </TagBlock>
-      <TimerContainer start={start} end={end}/>
+      <TimerContainer start={start} end={end} />
       <ContentDiv>
-        <TypingWindow tabIndex={0} onKeyPress={ !end ? handleKeyPress : null} onKeyDown={ !end ? deleteKeyDown : null}> 
+        <TypingWindow
+          tabIndex={0}
+          onKeyPress={!end ? handleKeyPress : null}
+          onKeyDown={!end ? deleteKeyDown : null}
+        >
           {chList.map((ch, index) => (
             <span
               className={`${
@@ -158,10 +163,11 @@ const Source = ({ game, chList }) => {
       </ContentDiv>
       <DescriptionDiv>설명:{description}</DescriptionDiv>
       <LinkDiv>출처:{link}</LinkDiv>
-      { (point && end)
-          ? <Summary point={point} end={end}></Summary>
-          : <Keyboard />
-      }
+      {point && end ? (
+        <Summary point={point} end={end}></Summary>
+      ) : (
+        <Keyboard />
+      )}
     </SourceBlock>
   );
 };
